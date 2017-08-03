3e825ff752a1c84af678284d02bb8902791e9ce5
mmclau14 <michael.j.mclaughlin20@gmail.com>
2017-08-02 22:51:41 -0500

    moved negation outside of closure

--- valid-integer/index.js
+++ valid-integer/index.js
@@ -1,5 +1,6 @@
 var MAX_INTEGER = require('@timelaps/constants/max-integer');
 var isNumber = require('../number');
+var MIN_INTEGER = -MAX_INTEGER;
 module.exports = function isValidInteger(number) {
-    return isNumber(number) && number >= -MAX_INTEGER && number <= MAX_INTEGER;
+    return isNumber(number) && number >= MIN_INTEGER && number <= MAX_INTEGER;
 };
